13fce75391a540d9f5f1e529b0338c3144887b2a
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-08-16 00:25:49 +0600

    add: design card content

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,24 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Rating } from "@smastrom/react-rating";
 
-const Card = ({product}) => {
-    const {Product_Name, Product_Image, Description, Price, Category, Ratings, Product_Creation_date} = product;
+import "@smastrom/react-rating/style.css";
+
+const Card = ({ product }) => {
+  const {
+    Product_Name,
+    Product_Image,
+    Description,
+    Price,
+    Category,
+    Ratings,
+    Product_Creation_date,
+  } = product;
+
+  const date = new Date(Product_Creation_date);
+  const localTime = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  const dateTime = Product_Creation_date.split("T")[0] + ", " + localTime;
   return (
-      <div className="card card-compact bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{Product_Name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+    <div className="card card-compact bg-base-100 shadow-xl h-full">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body flex flex-col justify-between">
+        <div>
+          <div className="flex gap-3 items-center">
+            <h2 className="card-title">{Product_Name}</h2>
+            <Rating style={{ maxWidth: 120 }} value={Ratings} readOnly />
+          </div>
+          <h3 className="font-medium text-neutral-400">{dateTime}</h3>
+          <h5 className="text-lg">{Description}</h5>
+          <div className="card-actions my-5">
+            <div className="badge badge-outline">{Category}</div>
           </div>
         </div>
+        <div className="card-actions justify-between items-center mt-auto">
+          <h3 className="text-2xl font-semibold text-orange-500">$ {Price}</h3>
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
       </div>
+    </div>
   );
 };
 
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -14,7 +14,7 @@ const Main = () => {
     return (
         <div className="container mx-auto px-3 md:px-6 lg:px-12">
             <h1 className="text-3xl font-bold text-blue-600 my-8">EzyCart</h1>
-            <div className="grid grid-cols-3 gap-6"> 
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> 
                 {
                     products.map((product, index) => <Card key={index} product={product}></Card>)
                 }
