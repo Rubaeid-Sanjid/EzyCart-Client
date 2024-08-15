211b720f3415ba8be417f737a64c354350a820d2
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-08-15 20:58:30 +0600

    eslint fixed

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import { useEffect } from "react";
 
 const Main = () => {
+    const [products, setProducts] = useState([]);
+
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
-        .then(data => console.log(data)
+        .then(data => setProducts(data)
         )
     },[])
     return (
         <div>
             <h1 className="text-3xl font-bold text-blue-600">EzyCart</h1>
+            <h3>{products.length}</h3>
         </div>
     );
 };
