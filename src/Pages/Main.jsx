e789da0f8dde797992d0ea52955fc48fd7bc3e8c
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-08-17 17:25:56 +0600

    add: search functionality

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -5,8 +5,10 @@ import Card from "../Components/Card";
 const Main = () => {
   const [products, setProducts] = useState([]);
   const [totalProducts, setTotalProducts] = useState(0);
-
   const [currentPage, setCurrentPage] = useState(1);
+
+  const [searchText, setSearchText] = useState("");
+  console.log(searchText);
 
   const numberOfItemsPerPage = 6;
 
@@ -16,20 +18,19 @@ const Main = () => {
   for (let i = 1; i <= totalNumberOfPages; i++) {
     pages.push(i);
   }
-  console.log(pages);
 
   useEffect(() => {
     fetch(
       `http://localhost:5000/products?page=${
         currentPage - 1
-      }&items=${numberOfItemsPerPage}`
+      }&items=${numberOfItemsPerPage}&search=${searchText}`
     )
       .then((res) => res.json())
       .then((data) => {
         setProducts(data.products);
         setTotalProducts(data.totalProducts);
       });
-  }, [currentPage, numberOfItemsPerPage]);
+  }, [currentPage, numberOfItemsPerPage, searchText]);
 
   const handlePrevBtn = () => {
     if (currentPage > 1) {
@@ -43,13 +44,33 @@ const Main = () => {
   };
   return (
     <div className="container mx-auto px-3 md:px-6 lg:px-12">
-      <h1 className="text-3xl font-bold text-blue-600 my-8">EzyCart</h1>
+      <h1 className="text-3xl font-bold text-orange-500 my-8">EzyCart</h1>
+
+      {/* Filtering section */}
+      <div className="my-8">
+        <div className="flex items-center">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered input-warning w-full max-w-xs"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              setCurrentPage(1);
+            }}
+          />
+          {/* <button className="btn bg-orange-500 text-white">Search</button> */}
+        </div>
+      </div>
+
+      {/* Card section */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product, index) => (
           <Card key={index} product={product}></Card>
         ))}
       </div>
 
+      {/* Pagination buttons */}
       <div className="text-center my-8">
         <button
           className="btn"
@@ -62,7 +83,9 @@ const Main = () => {
           <button
             key={page}
             onClick={() => setCurrentPage(page)}
-            className={`${currentPage === page && "bg-orange-500 text-white"} btn mx-1`}
+            className={`${
+              currentPage === page && "bg-orange-500 text-white"
+            } btn mx-1`}
           >
             {page}
           </button>
